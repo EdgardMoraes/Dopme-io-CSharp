c14762971c1489701f349bd1049f8510a59583a9
Edgard <edgardmoraes@icloud.com>
2026-04-08 20:10:55 +0100

    feat15.2.2: criar modelo livro

---
+++ Dopme-io-CSharp/BibliotecaAPI/Models/Livro.cs
@@ -0,0 +1,14 @@
+﻿namespace BibliotecaAPI.Models;
+
+public class Livro
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; } = string.Empty;
+    public string ISBN { get; set; } = string.Empty;
+    public int AnoPublicacao { get; set; }
+    public int CategoriaId { get; set; }  //forenstkey
+    public int AutorId { get; set; }      //forenstkey
+// Navegação
+    public Categoria Categoria { get; set; } = null!;
+    public Autor Autor { get; set; } = null!;
+}

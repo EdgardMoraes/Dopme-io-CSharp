42696ecf70742107edbbd05c89c591d4ef261f10
Edgard <edgardmoraes@icloud.com>
2026-04-08 20:06:18 +0100

    Created project "bibliotecaApi"

---
+++ Dopme-io-CSharp/BibliotecaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaAPI.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        //DbSets serão adicionados depois
+    }
+}

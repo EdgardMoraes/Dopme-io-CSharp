f4cf766c728d92f5354ceff15bc49e8f3206dd7f
Edgard <edgardmoraes@icloud.com>
2026-04-13 19:08:33 +0100

    feat/OnModelCreating

--- Dopme-io-CSharp/BibliotecaAPI/Data/ApplicationDbContext.cs
+++ Dopme-io-CSharp/BibliotecaAPI/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BibliotecaAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BibliotecaAPI.Data;
 
@@ -7,5 +8,52 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
         //DbSets serão adicionados depois
+    }
+
+    public DbSet<Livro> Livros { get; set; }
+    public DbSet<Autor> Autores { get; set; }
+    public DbSet<Categoria> Categorias { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Livro>(e =>
+        {
+            e.ToTable("Livros");
+            e.HasKey(l => l.Id);
+            e.Property(l => l.Titulo)
+                .IsRequired()
+                .HasMaxLength(100);
+            e.Property(l => l.ISBN)
+                .IsRequired()
+                .HasMaxLength(20);
+            e.Property(l => l.AnoPublicacao)
+                .IsRequired()
+                .HasMaxLength(4);
+        });
+
+        modelBuilder.Entity<Autor>(a =>
+        {
+            a.ToTable("Autores");
+            a.HasKey(a => a.Id);
+            a.Property(a => a.Nome)
+                .IsRequired()
+                .HasMaxLength(50);
+            a.Property(a => a.DataNascimento)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+        });
+
+        modelBuilder.Entity<Categoria>(c =>
+        {
+            c.ToTable("Categorias");
+            c.HasKey(c => c.Id);
+            c.Property(c => c.Nome)
+                .IsRequired()
+                .HasMaxLength(50);
+            c.Property(c => c.Descricao)
+                .HasMaxLength(100);
+        });
     }
 }

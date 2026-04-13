93a4119cf878541d95d36f09f61eada66684ea7f
Edgard <edgardmoraes@icloud.com>
2026-04-13 19:07:05 +0100

    feat/ClasseAutor

--- Dopme-io-CSharp/BibliotecaAPI/Models/Autor.cs
+++ Dopme-io-CSharp/BibliotecaAPI/Models/Autor.cs
@@ -1,9 +1,18 @@
-﻿namespace BibliotecaAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace BibliotecaAPI.Models;
+
+[Table("Autores")]
 public class Autor
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
     public string Nome { get; set; } = string.Empty;
+    [Required]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public DateTime DataNascimento { get; set; }
 // Navegação
     public List<Livro> Livros { get; set; } = new();

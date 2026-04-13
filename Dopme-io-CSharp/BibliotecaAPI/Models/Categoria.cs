52413bd17277d1d865c263a8730c4c7a58390505
Edgard <edgardmoraes@icloud.com>
2026-04-13 19:07:21 +0100

    feat/ClasseCategoria

---
+++ Dopme-io-CSharp/BibliotecaAPI/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace BibliotecaAPI.Models;
+
+public class Categoria
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+    public string Descricao { get; set; } = string.Empty;
+// Navegação
+    public List<Livro> Livros { get; set; } = new();
+}
